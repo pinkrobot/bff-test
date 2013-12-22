62d0a7a95f60f72fca402665c5bcbc527327d578
Tom Collier <collier@verticalbrands.com>
2013-12-21 20:27:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BffTest::Application.config.secret_key_base = 'e6f17a8353fcfbde8481b83e4eda8a1c525c3c8682117e986535f44a23d58a0234affcf0641f97b4d8bde7fbad7142f253a5fd0e9ba459685a124cb80623540a'
